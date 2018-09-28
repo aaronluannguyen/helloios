a2da5ebbb901eb01713ed89a2f7022b691e4cfeb
aaronluannguyen <aaronluannguyen@gmail.com>
2018-09-28 00:06:14 -0700

    Initialized hellios project

---
+++ helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Aaron Nguyen on 9/28/18.
+//  Copyright © 2018 Aaron Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
